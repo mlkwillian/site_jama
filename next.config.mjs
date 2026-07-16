aa9e11d2282460cc528aae6c3ca5e98987e94a40
mlkwillian <williangomese10@gmail.com>
2026-07-16 20:22:58 -0300

    1

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
-/** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
+
+  images: {
+    unoptimized: true,
+  },
+
+  basePath: "/site_jama",
+  assetPrefix: "/site_jama",
+
   allowedDevOrigins: ["192.168.0.17"],
 };
 
