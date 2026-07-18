a9eb093942cd51528ceb9846a46f58dc3f2c19c8
mlkwillian <williangomese10@gmail.com>
2026-07-17 22:45:28 -0300

    Configure GitHub Actions deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,21 +1,17 @@
+/** @type {import('next').NextConfig} */
+
 const isProd = process.env.NODE_ENV === "production";
 
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  trailingSlash: true,
 
   images: {
     unoptimized: true,
   },
 
   basePath: isProd ? "/site_jama" : "",
-  assetPrefix: isProd ? "/site_jama/" : "",
-
-  env: {
-    NEXT_PUBLIC_BASE_PATH: isProd ? "/site_jama" : "",
-  },
-  
-  allowedDevOrigins: ["192.168.0.17"],
+  assetPrefix: isProd ? "/site_jama" : "",
 };
 
 export default nextConfig;
