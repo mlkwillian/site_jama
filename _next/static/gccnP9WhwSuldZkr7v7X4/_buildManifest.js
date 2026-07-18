8f2c2b0c3437553364b40a5d6ebaf8d9e3c43aae
mlkwillian <williangomese10@gmail.com>
2026-07-17 21:59:09 -0300

    Updates

--- _next/static/gccnP9WhwSuldZkr7v7X4/_buildManifest.js
+++ _next/static/gccnP9WhwSuldZkr7v7X4/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/site_jama//_next/:path+",
+        "destination": "/site_jama/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
