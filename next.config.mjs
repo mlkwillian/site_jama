94a1fcec83f57e7a6fea20f37c769922c2aab552
mlkwillian <williangomese10@gmail.com>
2026-07-22 19:02:22 -0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,6 @@
-/** @type {import('next').NextConfig} */
-
-const isProd = process.env.NODE_ENV === "production";
-
 const nextConfig = {
-  output: "export",
-  trailingSlash: true,
-
   images: {
     unoptimized: true,
-  },
-
-  basePath: isProd ? "/site_jama" : "",
-  assetPrefix: isProd ? "/site_jama" : "",
-
-  env: {
-    NEXT_PUBLIC_BASE_PATH: isProd ? "/site_jama" : "",
   },
 };
 
